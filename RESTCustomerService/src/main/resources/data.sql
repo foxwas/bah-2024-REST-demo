a15e5ec69d8d33a755c9b14736e47cd38f9aaf9e
saravanankuppusamy <37090060+saravanankuppusamy@users.noreply.github.com>
2024-10-15 21:09:31 -0400

    Add files via upload

---
+++ RESTCustomerService/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('lexi', 'pass', 'lexi@bah.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('jane', 'pass', 'jane@bah.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('kyle', 'pass', 'kyle@bah.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('zach', 'pass', 'zach@bah.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('clement', 'pass', 'cleament@bah.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('rayyan', 'pass', 'rayyan@bah.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('stuart', 'pass', 'stuart@bah.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('winston', 'pass', 'winston@bah.com');
